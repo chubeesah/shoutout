14ec8e07a82e3de068947443fdccd3e4e4ab52b9
Chubes <Chubes@andrew-mercers-macbook.local>
2015-02-09 16:52:03 -0500

    solved first error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params)
+    render :index
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "User deleted!"
+  end
+
+  private
+  def user_params
+     params.require(:user).permit(:username, :password)
+  end
 end
