9b4a5902d44a915c3767a3fcadc71dcffe2b0be8
Dylan Dechant <dylan.dechant@gmail.com>
2015-02-09 15:32:45 -0500

    forgot to add user_id

--- db/migrate/20150209201346_create_shouts.rb
+++ db/migrate/20150209201346_create_shouts.rb
@@ -2,6 +2,7 @@ class CreateShouts < ActiveRecord::Migration
   def change
     create_table :shouts do |t|
       t.string :yell
+      t.integer :user_id
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20150209201346) do
 
   create_table "shouts", force: :cascade do |t|
     t.string   "yell"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
